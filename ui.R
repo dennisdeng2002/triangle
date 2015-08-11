41aa5355f3f8e4abdd6809924967158a0d09cd5c
Dennis Deng <dennisdeng2002@Dennis-Dengs-MacBook-Pro.local>
2015-08-11 19:21:26 -0400

    Tie-line features, reactive function, beta point adding number 2

--- ui.R
+++ ui.R
@@ -1,16 +1,4 @@
-#### ui.R
-# Issues: 
-### LOAD LIBRARIES ###
-library(shiny)         # SHINY - Web interface
-library(ggtern)        # Plotting ternary diagram
-library(gridSVG)       # Converting Image to SVG
-library(svgPanZoom)    # Pan and Zoom function
-library(rhandsontable) # Table interface
-library(tools)
-library(grid)         
-library(markdown)      # Used for Help
 
-<<<<<<< HEAD
 library(shiny)
 library(ggtern)
 library(gridSVG)
@@ -56,43 +44,4 @@ shinyUI(
 #                resetOnNew = TRUE))
   ))),
   tabPanel("Help")
-=======
-shinyUI(
-    navbarPage("LLE",
-          tabPanel("Ternary",
-          headerPanel("Ternary Diagram"),
-                            
-          # Sidebar
-            sidebarPanel(
-            # Upload CSV file  
-              fileInput("file1", "Upload Data:", accept = c('.csv',
-                                                            '.tsv')),
-            # Generate table
-              rHandsontableOutput("hot"),
-            # Commit changes to table
-              actionButton("plot_button","Graph")),
-            
-            # verbatimTextOutput("info"),
-          
-            
-            # Generate plot
-            mainPanel(
-              column(width = 12, class = "well",
-               plotOutput("TernPlot", 
-               width = "100%",
-               click = "plot_click",
-               dblclick = "plot_dblclick",
-               hover = "plot_hover",
-               brush = brushOpts(
-                id = "plot_brush",
-                resetOnNew = TRUE)))
-              )),
-#                  ))
-          tabPanel("Help",
-             headerPanel("Ternary Diagram"),
-             fluidRow(
-               column(4,
-                      includeMarkdown("HELP.md")
-               )))
->>>>>>> c79f4e669bbfd731ddcddc035cde073a0b9ad3d0
 ))
